3fee4275a67d56d852b93992f63d4475597b8474
Hardip Chander <hardipchander@gmail.com>
2022-10-31 15:39:37 -0400

    Added title styled it, added button style it, now work on logic

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import './App.css';
 import Card from './Componets/Card';
 
 function App() {
+  function onHandleReshuffle() {
+    // Just to test for now
+    console.log("Pressed the Reshuffle");
+  }
+
   return (
     <div className="App">
-
       <div className="Header">
-
+        <h1 className='title'>A picture is worth a thousand words, but a memory is priceless</h1>
+        <button className='buttonReset' onClick={onHandleReshuffle}>Reshuffle</button>
       </div>
-
+      
       <div className='rowOne'>
         <Card number={1}/>
         <Card number={2}/>
@@ -22,7 +27,6 @@ function App() {
         <Card number={7}/>
         <Card number={8}/>
       </div>
-
     </div>
   );
 }

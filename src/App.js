ce0d87061d2683c9e10bf40baa25956a93627642
Hardip Chander <hardipchander@gmail.com>
2022-10-31 14:57:09 -0400

    Set up the layout(flexbox) for 2 card rows

--- src/App.js
+++ src/App.js
@@ -4,14 +4,25 @@ import Card from './Componets/Card';
 function App() {
   return (
     <div className="App">
-      <Card number={1}/>
-      <Card number={2}/>
-      <Card number={3}/>
-      <Card number={4}/>
-      <Card number={5}/>
-      <Card number={6}/>
-      <Card number={7}/>
-      <Card number={8}/>
+
+      <div className="Header">
+
+      </div>
+
+      <div className='rowOne'>
+        <Card number={1}/>
+        <Card number={2}/>
+        <Card number={3}/>
+        <Card number={4}/>
+      </div>
+
+      <div className='rowTwo'>
+        <Card number={5}/>
+        <Card number={6}/>
+        <Card number={7}/>
+        <Card number={8}/>
+      </div>
+
     </div>
   );
 }

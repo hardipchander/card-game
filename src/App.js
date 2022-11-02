56d024a2fed17af5f57612802342edfa753e3f30
Hardip Chander <hardipchander@gmail.com>
2022-11-02 15:54:07 -0400

    Implemented randomness in the reshuffle button

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
 import './App.css';
+import { useState } from 'react';
 import Card from './Componets/Card';
 
 function App() {
+  // Add state for storing information about the game
+  const [numsForCards, setCards]=useState([1,2,3,4,1,2,3,4].sort(()=> Math.random() -0.5));
 
   function onHandleReshuffle() {
-    // Just to test for now
-    console.log("Pressed the Reshuffle");
+    // Randomize with random method
+    const randomizeCard=[...numsForCards].sort(()=> Math.random() -0.5);
+    setCards(randomizeCard);
   }
 
-  // Need this to add randomness
-  const values=[1,2,3,4,5,6,7,8];
-
   return (
-
     <div className="App">
       <div className="Header">
         <h1 className='title'>A picture is worth a thousand words, but a memory is priceless</h1>
         <button className='buttonReset' onClick={onHandleReshuffle}>Reshuffle</button>
       </div>
       
-      <div className='rowOne'>
-        <Card number={1}/>
-        <Card number={2}/>
-        <Card number={3}/>
-        <Card number={4}/>
+      <div className='rowOne' >
+        <Card number={numsForCards[0]} id={numsForCards[0]}   />
+        <Card number={numsForCards[1]} id={numsForCards[1]}  />
+        <Card number={numsForCards[2]} id={numsForCards[2]}  />
+        <Card number={numsForCards[3]} id={numsForCards[3]}  />
       </div>
 
-      <div className='rowTwo'>
-        <Card number={1}/>
-        <Card number={2}/>
-        <Card number={3}/>
-        <Card number={4}/>
+      <div className='rowTwo' >
+        <Card number={numsForCards[4]} id={numsForCards[4]}   />
+        <Card number={numsForCards[5]} id={numsForCards[5]}  />
+        <Card number={numsForCards[6]} id={numsForCards[6]}  />
+        <Card number={numsForCards[7]} id={numsForCards[7]}  />
       </div>
     </div>
   );
--- src/Componets/Card.js
+++ src/Componets/Card.js
@@ -1,29 +1,31 @@
 import React from 'react';
+import App from '../App';
 import '../Styles/Card.css';
 
 class Card extends React.Component {
     constructor(props){
         super(props);
         this.state={
-            isFlipped:false  
+            isFlipped: true,  
         };
     }
 
-    pressedCard=() => {
-        this.setState(state => ({isFlipped: !state.isFlipped}));
+    pressed=()=> {
+        this.setState( prevState =>({isFlipped : !prevState.isFlipped}));
     };
+    
 
     render() {
         if(this.state.isFlipped===false) {
             return (
-                <div className="Hidden" onClick={this.pressedCard}>
+                <div className="Hidden" onClick={this.pressed}>
                     <p></p>
                 </div>
             );
         }
         else {
             return (
-                <div className='Actual-Card' onClick={this.pressedCard}>
+                <div className='Actual-Card' onClick={this.pressed}>
                     <p>#{this.props.number}</p>
                 </div>
             );
